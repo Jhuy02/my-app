eb34cd63982a79b3eac062b5d87c8a27bd223230
Jhuy02 <ttruonghuy2502@gmail.com>
2024-04-12 11:08:14 +0700

    delete api

--- src/app/chi-tiet-san-pham/page.jsx
+++ src/app/chi-tiet-san-pham/page.jsx
@@ -1,16 +1,15 @@
 import TransitionLink from "@/components/TransitionLink";
 
 export default async function page() {
-  const res = await fetch(
-    "https://astro-api.okhub-tech.com/wp-json/wp/v2/pages/295"
-  );
-  const data = await res.json();
-  return (
-    <main>
-      <TransitionLink label="home" href={"/"} />
-      {data?.acf?.blog?.map((e, index) => (
-        <div dangerouslySetInnerHTML={{ __html: e?.blog }} key={index} />
-      ))}
-    </main>
-  );
+  // const res = await fetch(
+  // );
+  // const data = await res.json();
+  // return (
+  //   <main>
+  //     <TransitionLink label="home" href={"/"} />
+  //     {data?.acf?.blog?.map((e, index) => (
+  //       <div dangerouslySetInnerHTML={{ __html: e?.blog }} key={index} />
+  //     ))}
+  //   </main>
+  // );
 }
